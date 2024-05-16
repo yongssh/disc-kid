93d65d4d349b2061adc4b1618f833a5ef313c9dc
yongssh <yongyu.yy.huang@gmail.com>
2024-05-15 21:48:05 -0500

    added conditional visibility function; will display "RECALL WARNING" if toggled. still need to implement the actual recall result

--- popup.js
+++ popup.js
@@ -4,10 +4,39 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('aboutKID').addEventListener('click', function() {
         chrome.tabs.create({ url: "https://kidsindanger.org" });
     });
-    
+
+    // email pop up
     document.getElementById("contactKID").addEventListener('click', function(){
-        // email pop up
         var mailtoUrl = "mailto:email@kidsindanger.org" ;
         window.open(mailtoUrl);
-    })
+    
+    });
+   
+
 });
+
+
+// visible only if recall detected (similarity score high enough
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Assume this variable determines whether the recall warning is triggered
+    let recallBool = false;
+
+    // **TO-DO**:
+    // need to be implemented:
+    // if recall-matching returns results, then set recallBool to True
+    
+
+    // Function to toggle the visibility of the warning div based on the variable
+    function toggleContentVisibility() {
+        const conditionalContent = document.getElementById('warning-triggered');
+        if (recallBool) {
+            conditionalContent.style.display = 'block'; // show recall warning
+        } else {
+            conditionalContent.style.display = 'none'; // hide recall warning
+        }
+    }
+    // Call the function initially to set the initial visibility state
+    toggleContentVisibility();
+});
+
