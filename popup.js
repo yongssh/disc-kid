f17ec79c78bbe5af981b4b4f3e7b1351b9f90529
yongssh <yongyu.yy.huang@gmail.com>
2024-05-15 21:25:00 -0500

    small changes

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add click event listener to the "Open Wordle" button
+
+    // links "about KID" button to KID website
     document.getElementById('aboutKID').addEventListener('click', function() {
-        // Open a new tab to the Wordle game
         chrome.tabs.create({ url: "https://kidsindanger.org" });
     });
+    
     document.getElementById("contactKID").addEventListener('click', function(){
         // email pop up
         var mailtoUrl = "mailto:email@kidsindanger.org" ;
