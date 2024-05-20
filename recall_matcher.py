6a6e891499bc1ea71260caee64ce4f2dbea69060
Jonathan Lai <jonathanlai10@gmail.com>
2024-05-20 18:30:07 -0500

    Update recall_matcher.py

--- recall_matcher.py
+++ recall_matcher.py
@@ -4,113 +4,98 @@ import re
 from sentence_transformers import SentenceTransformer
 from sentence_transformers.util import pytorch_cos_sim
 
-# ------------------------------
+# -----------------------------
 
-def text_cleaner(text): # this function just standardizes the text to be more uniform for comparison
+def text_cleaner(text):
     lowercased_text = text.lower()
     remove_punctuation_table = str.maketrans('', '', string.punctuation)
     normalized_text = lowercased_text.translate(remove_punctuation_table)
-    normalized_text = re.sub('(\s+)(a|an|and|the|with)(\s+)', '\1\3', normalized_text)   # remove article and filler words, can add other things to remove as well
+    normalized_text = re.sub('(\s+)(a|an|and|the|with)(\s+)', '\1\3', normalized_text)
     return normalized_text
 
 # ------------------------------
 
-def string_comparer1(text1, text2): # uses the ratio of common unique words between two strings as the similarity
-    
+def string_comparer1(text1, text2):
     text1_words = set(text_cleaner(text1).split())
     text2_words = set(text_cleaner(text2).split())
-    
     common_words = text1_words.intersection(text2_words)
-    
     similarity_score = round(len(common_words) / min([len(text1_words), len(text2_words)]), 3)
-    
     return similarity_score
 
 # ------------------------------
 
-transformer = SentenceTransformer('distilbert-base-nli-mean-tokens')                    # This is the transformer that should be instated ONCE prior to function call
-def string_comparer2(text1, text2, model=transformer): # uses transformer embeddings and cosine similarity to compute similarity
-    
-    # model = SentenceTransformer('distilbert-base-nli-mean-tokens')                    # Used to be contained here but it increases time cost of calling function significantly
+transformer = SentenceTransformer('distilbert-base-nli-mean-tokens')
 
+def string_comparer2(text1, text2, model=transformer):
     text1_clean = text_cleaner(text1)
     text2_clean = text_cleaner(text2)
-    
     text1_encoding = model.encode(text1_clean)
     text2_encoding = model.encode(text2_clean)
-    
     similarity_score = round(float(pytorch_cos_sim(text1_encoding, text2_encoding)), 3)
-
     return similarity_score
 
+# ------------------------------
+
 def query_cpsc(manufacturer_key=None, productname_key=None):
-    
+    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
     base_url = "https://www.saferproducts.gov/RestWebServices/Recall?format=json"
-    
     query_string = ""
-    
     if manufacturer_key:
         query_string += f"&Manufacturer={manufacturer_key}"
     if productname_key:
         query_string += f"&ProductName={productname_key}"
-        
     query_results = requests.get(base_url + query_string, headers=header).json()
-        
     return query_results
 
-# ------------------------------------------------ 
+# ------------------------------------------------
 
-def RecallSimilarity(recall): # add amazon info as param later?
+def RecallSimilarity(recall, amazon_info):
     
-    # calculate description similarity if a description exists for both
     desc_similarity = None
-    recall_desc = recall['Description']
-    if recall_desc and amazon_desc:
-        desc_similarity = string_comparer2(recall_desc, amazon_desc)  # compare by cosine similarity
+    recall_desc = recall.get('Description', '')
 
-    # calculate product name similarity if product name exists for both
+    if recall_desc and (amazon_info['Description'] != 'Description not found'):
+        desc_similarity = string_comparer2(recall_desc, amazon_info['Description'])
+
     product_name_max_similarity = None
-    for product in recall['Products']:
-        if product['Name'] and amazon_title:
-            product_name_similarity = string_comparer2(product['Name'], amazon_title)  # compare by cosine similarity
-            if product_name_similarity > float(product_name_max_similarity or 0): 
+
+    for product in recall.get('Products', []):
+        if product.get('Name') and (amazon_info['Product Name'] != 'Product name not found'):
+            product_name_similarity = string_comparer2(product['Name'], amazon_info['Product Name'])
+            if product_name_similarity > float(product_name_max_similarity or 0):
                 product_name_max_similarity = product_name_similarity
-                
-    # calculate manufacturer name similarity if exists for both
+
     manufacturer_max_similarity = None
-    for manufacturer in recall['Manufacturers']:
-        if manufacturer['Name'] and amazon_manufacturer:
-            manufacturer_similarity = string_comparer1(manufacturer['Name'], amazon_manufacturer)  # compare by common words
+    
+    for manufacturer in recall.get('Manufacturers', []):
+        if manufacturer.get('Name') and (amazon_info['Brand'] != 'Brand not found'):
+            manufacturer_similarity = string_comparer1(manufacturer['Name'], amazon_info['Brand'])
             if manufacturer_similarity > float(manufacturer_max_similarity or 0):
                 manufacturer_max_similarity = manufacturer_similarity
-    
+
     recall_similarity_score = 0
-    
     valid_scores = list(filter(None, [desc_similarity, product_name_max_similarity, manufacturer_max_similarity]))
-    
+
     for score in valid_scores:
         if score == desc_similarity:
             recall_similarity_score += score
         elif score == product_name_max_similarity:
-            recall_similarity_score += score*2
+            recall_similarity_score += score * 2
         elif score == manufacturer_max_similarity:
-            recall_similarity_score += score*2
-            
-    # normalize by number of valid scores 
-    recall_similarity_score = round( recall_similarity_score * (3/len(valid_scores))**0.5 , 3)
-    
+            recall_similarity_score += score * 2
+
+    recall_similarity_score = round(recall_similarity_score * (3 / len(valid_scores)) ** 0.5, 3)
     return recall_similarity_score
 
 # --------------------------------------------------------
 
-def GetMatchingRecalls(query_results, similarity_threshold=2.5):
+def GetMatchingRecalls(query_results, amazon_info, similarity_threshold=2.0):
     matching_recalls = []
-
     for recall in query_results:
-        recall['SimilarityScore'] = RecallSimilarity(recall)    # add similarity score to recall dictionary
-        if recall['SimilarityScore'] > similarity_threshold:      # arbitrary default threshold of 2.5 for similarity score
+        recall['SimilarityScore'] = RecallSimilarity(recall, amazon_info)
+        if recall['SimilarityScore'] > similarity_threshold:
             matching_recalls.append(recall)
-
+    matching_recalls.sort(key=lambda x: x['SimilarityScore'], reverse=True)
     return matching_recalls
 
 # ------------------------------------------------------- EXAMPLE USAGE FOR GEL BLASTER SURGE
